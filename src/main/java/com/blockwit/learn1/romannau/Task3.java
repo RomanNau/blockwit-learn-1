e6a1c93bac51633e972b45d2e9d4afa5ba395164
RomanNaun <romoolay@gmail.com>
2021-12-24 13:27:01 +0300

    Task3 finished

---
+++ src/main/java/com/blockwit/learn1/romannau/Task3.java
@@ -0,0 +1,15 @@
+//У вас есть список имен: Alice, Bob, John.
+//Вывести его в прямом порядке.
+//Сдать до конца завтрашнего дня.
+
+
+package com.blockwit.learn1.romannau;
+
+public class Task3 {
+    public static void main(String[] args) {
+        String[] Names = new String[] {"Alice", "Bob", "John"};
+        for (int i=0; i<3; i++) {
+            System.out.println(Names[i]);
+        }
+    }
+}
