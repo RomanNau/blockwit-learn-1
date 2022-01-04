993baa2bb80d5c258e3b20320614a4583f32284f
RomanNaun <romoolay@gmail.com>
2022-01-04 12:51:36 +0300

    Task12 finished

---
+++ src/main/java/com/blockwit/learn1/romannau/Task12.java
@@ -0,0 +1,16 @@
+//Необходимо вывести десять раз числа от 50 до 100
+
+
+
+
+package com.blockwit.learn1.romannau;
+
+public class Task12 {
+    public static void main(String[] args) {
+        for(int i = 0; i < 10; i++) {
+            for(int a = 50; a < 101; a++) {
+                System.out.println( a );
+            }
+        }
+    }
+}
