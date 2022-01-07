d592bd7162aa7093b4e177fdc92ad8a35cab5869
RomanNaun <romoolay@gmail.com>
2022-01-07 22:16:59 +0300

    Task23 finished

---
+++ src/main/java/com/blockwit/learn1/romannau/Task23.java
@@ -0,0 +1,17 @@
+//Есть массив данных:
+//65,90,61,33,45
+//Вывести три первых числа
+
+
+
+
+package com.blockwit.learn1.romannau;
+
+public class Task23 {
+    public static void main(String[] args) {
+        int[] array = {65,90,61,33,45};
+        for ( int i = 0; i < 3; i++) {
+            System.out.println(array[i]);
+        }
+    }
+}
