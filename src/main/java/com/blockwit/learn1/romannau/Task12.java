a0b52932112b0b5f09b00346a792df6531f9a6be
RomanNaun <romoolay@gmail.com>
2022-01-08 13:08:52 +0300

    Task26 finished

--- src/main/java/com/blockwit/learn1/romannau/Task12.java
+++ src/main/java/com/blockwit/learn1/romannau/Task12.java
@@ -8,7 +8,7 @@ package com.blockwit.learn1.romannau;
 public class Task12 {
     public static void main(String[] args) {
         for(int i = 0; i < 10; i++) {
-            for(int a = 50; a < 101; a++) {
+            for(int a = 50; a <= 100; a++) {
                 System.out.println( a );
             }
         }
