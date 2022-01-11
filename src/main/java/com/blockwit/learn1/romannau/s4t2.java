0705d21882a58d540dc927a01cf1244fa6437006
RomanNaun <romoolay@gmail.com>
2022-01-11 16:05:20 +0300

    s4t2 finished

---
+++ src/main/java/com/blockwit/learn1/romannau/s4t2.java
@@ -0,0 +1,22 @@
+//s4t2
+//Написать программу, которая спрашивает у пользователя 5 имен.
+// А затем выводит все эти имена.
+
+package com.blockwit.learn1.romannau;
+
+import java.util.Scanner;
+
+public class s4t2 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String[] names = new String[5];
+        for (int i = 0; i < names.length; i++) {
+            System.out.println("Введите ваше имя:  ");
+            names[i] = in.nextLine();
+        }
+        for(int i =0; i < names.length; i++) {
+            System.out.println(names[i]);
+        }
+        in.close();
+    }
+}
