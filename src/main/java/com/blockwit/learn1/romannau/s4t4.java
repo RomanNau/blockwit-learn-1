83d868f04ca111be633144d5e14bd2800718a8fe
RomanNaun <romoolay@gmail.com>
2022-01-11 21:38:20 +0300

    s4t4 finished

---
+++ src/main/java/com/blockwit/learn1/romannau/s4t4.java
@@ -0,0 +1,37 @@
+//s4t4
+//Написать программу, которая спрашивает, сколько у Вас друзей.
+//А потом спрашивает имена этих друзей. Затем выводит имена друзей в обратном порядке.
+
+
+package com.blockwit.learn1.romannau;
+
+import java.util.Scanner;
+
+public class s4t4 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int countfreds;
+        System.out.println("Ввести точное кол-во ваших друзей :  ");
+        int countfrends = in.nextInt();
+        in.nextLine();
+        if(countfrends>0){
+
+        String[] names = new String[countfrends];
+        for (int i = 0; i < names.length; i++) {
+            System.out.println("Введите имя друга:  ");
+            names[i] = in.nextLine();
+
+        }
+        for (int k = names.length-1; k >= 0; k--) {
+            System.out.println(names[k]);
+            }
+        } else {
+            System.out.println("У вас нет друзей, Вы С о Ц и О п а Т!!! Опасны для общества!! ");
+        }
+
+        in.close();
+    }
+}
+
+
+
