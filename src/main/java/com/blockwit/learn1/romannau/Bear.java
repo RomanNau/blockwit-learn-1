9db1f0fc58dee39955a597d6410b195b448a1ce9
RomanNaun <romoolay@gmail.com>
2022-01-16 17:12:16 +0300

    task completed

---
+++ src/main/java/com/blockwit/learn1/romannau/Bear.java
@@ -0,0 +1,15 @@
+//Написать класс животного. У него должны быть: дата рождения, цвет, название.
+// Также написать метод, который возвращает сколько животному лет.
+
+
+package com.blockwit.learn1.romannau;
+
+public class Bear {
+    int dateBirth;
+    String color;
+    String name;
+    public int getYear() {
+        int year = 2022-dateBirth;
+        return year;
+    }
+}
