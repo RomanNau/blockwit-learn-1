8c267c1831278ca451f0de9acc785d53a4ac5b79
RomanNaun <romoolay@gmail.com>
2022-02-06 16:12:07 +0300

    Task accomplished

---
+++ src/main/java/com/blockwit/learn1/romannau/lession6/ProtoList1.java
@@ -0,0 +1,24 @@
+//Написать класс ProtoList1 в котором будет поле array типа массив целых чисел.
+// Написать метод create который принимает размер и создает и присваивает полю array пустой массив целых чисел указанного размера.
+// Написать метод getSize который возращает длину массива в поле array.
+// Написать метод get, который принимает индекс( b ) и возращает элемент из массива array по указанному индексу.
+//
+
+package com.blockwit.learn1.romannau.lession6;
+
+public class ProtoList1 {
+    int[] array;
+
+    public void create(int size) {
+        int[] newArray = new int[size];
+        this.array = newArray;
+    }
+
+    public int getSize() {
+        return array.length;
+    }
+
+    public  int get(int b) {
+        return array[b];
+    }
+}
