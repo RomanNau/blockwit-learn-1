12a855400508fe10f17dcbf795e4d1f536b82b47
RomanNaun <romoolay@gmail.com>
2022-02-06 12:07:37 +0300

    added prerequisite for(arrayA.length > arrayB.length) line 24

--- src/main/java/com/blockwit/learn1/romannau/Helper.java
+++ src/main/java/com/blockwit/learn1/romannau/Helper.java
@@ -1,16 +1,11 @@
-//Сделать класс Helper. Внутри него метод printIntArray, который принимает массив чисел и распечатывает их.
-// Переписать предыдущую задачу Tu0, так чтобы она использовала метод printIntArray.
-//
-
-
 package com.blockwit.learn1.romannau;
 
 public class Helper {
 
-        public void printArray(int[] mass) {
-            for(int i = 0; i < mass.length; i++) {
-                System.out.print(mass[i] + ",");
-            }
+    public void printArray(int[] mass) {
+        for(int i = 0; i < mass.length; i++) {
+            System.out.print(mass[i] + ",");
+        }
 
     }
 }
